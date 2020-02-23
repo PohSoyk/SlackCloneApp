76ba1c9e0f29b8670de7639be05f878505e880dd
PoSo <hokuro.jump@gmail.com>
2020-02-23 17:21:45 +0900

    ログインモーダルを使ってGoogleログイン

--- pages/index.vue
+++ pages/index.vue
@@ -9,16 +9,7 @@ import { firebase } from "~/plugins/firebase";
 
 export default {
   methods: {
-    login() {
-      const provider = new firebase.auth.GoogleAuthProvider();
-      firebase.auth().signInWithPopup(provider)
-        .then((result) => {
-          const user = result.user
-          console.log(user)
-        }).catch(() => {
-          window.alert(error)
-        })
-    }
+
   }
 };
 </script>
